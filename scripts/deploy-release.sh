560eb37da3d8b3654347bcee52f68765f6ce5636
Volodymyr Galandzii <volodymyr.galandzii@onefootball.com>
2015-10-30 15:07:03 +0100

    fast deploy (only versions 18 and 21 left enabled - uncomment other if you need to deploy them too)

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -6,21 +6,22 @@
 set -e
 
 PROJECT=$(cd $(dirname "$0")/..; pwd)
+ARGUMENTS="--settings $PROJECT/settings.xml -D skipTests"
 
 echo "Building Robolectric..."
-cd "$PROJECT"; mvn -P release -D skipTests clean deploy
+cd "$PROJECT"; mvn ${ARGUMENTS} -P release clean deploy
 
-echo "Building shadows for API 16..."
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn -P release,android-16 -D skipTests clean deploy
-
-echo "Building shadows for API 17..."
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn -P release,android-17 -D skipTests clean deploy
+#echo "Building shadows for API 16..."
+#cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} -P release,android-16 clean deploy
+#
+#echo "Building shadows for API 17..."
+#cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} -P release,android-17 clean deploy
 
 echo "Building shadows for API 18..."
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn -P release,android-18 -D skipTests clean deploy
+cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} -P release,android-18 clean deploy
 
-echo "Building shadows for API 19..."
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn -P release,android-19 -D skipTests clean deploy
+#echo "Building shadows for API 19..."
+#cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} -P release,android-19 clean deploy
 
 echo "Building shadows for API 21..."
-cd "$PROJECT"/robolectric-shadows/shadows-core; mvn -P release,android-21 -D skipTests clean deploy
+cd "$PROJECT"/robolectric-shadows/shadows-core; mvn ${ARGUMENTS} -P release,android-21 clean deploy
